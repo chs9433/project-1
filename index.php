ef480c7dc8716157fa772ed22da655c2fdefd841
= <chs9433@g.harvard.edu>
2019-02-17 18:42:19 -0600

    Added index and images

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>DWA15 | Project 1</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+<h1>DWA15 Project 1</h1>
+<img src="#" width="100" height="200" />
+
+</body>
+</html>

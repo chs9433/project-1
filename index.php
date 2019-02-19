4042c8979c7ef9b81830e4956ba76717beac2709
= <chs9433@g.harvard.edu>
2019-02-18 19:22:26 -0600

    Corrected comment syntax to remove w3 validator warnings.

--- index.php
+++ index.php
@@ -35,9 +35,9 @@ function getQuote()
 	<title>DWA15 | Project 1</title>
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!--- Bootstrap 4 CSS --->
+  <!--Bootstrap 4 CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <!--- Custom Styling --->
+  <!--Custom Styling -->
   <style>
     body {
       padding:10px 5px;
@@ -57,37 +57,37 @@ function getQuote()
   </style>
 </head>
 <body>
-  <!--- Main Container --->
+  <!--Main Container -->
   <div class="container-fluid">
-    <!--- Project Card --->
+    <!--Project Card -->
     <div class="card text-left">
       <div class="row no-gutters">
         <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-          <!--- Profile Image --->
+          <!--Profile Image -->
           <img class="card-img dwa15-img" src="images/me1.jpg" alt="Christopher Sheppard">
         </div>
         <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-          <!--- Card Body --->
+          <!--Card Body -->
           <div class="card-body">
             <h2 class="cart-title" style="text-decoration: underline;">Christopher Sheppard</h2>
             <p class="card-text">
               Howdy! My name's Chris. I'm 33 and currently living in Dallas, Texas. DWA15 is my first course at Harvard Extension School. I'm a Tools & Automation Program Manager at a telecommunications company and am working towards an MLA degree in Software Engineering.Hobbies include playing piano, sax, drawing, playing video games, watching anime, working out and (trying to) cook.
             </p>
-          </div><!--- /Card Body --->
+          </div><!--/Card Body -->
         </div>
       </div>
-      <!--- Card Footer --->
+      <!--Card Footer -->
       <div class="card-footer text-muted">
         <h5>Random Quote</h5>
         <hr>
         <blockquote class="blockquote mb-2">
           <small>
-          <!--- PHP Quote Generator Function --->
+          <!--PHP Quote Generator Function -->
           <?php echo getQuote(); ?>
           </small>
         </blockquote>
-      </div><!--- /Card Footer --->
-    </div><!--- /Project Card --->
-  </div><!--- /Main Container --->
+      </div><!--/Card Footer -->
+    </div><!--/Project Card -->
+  </div><!--/Main Container -->
 </body>
 </html>
